27962f6ac42c9c0499018664343a7d05bc95bc48
Paul Brachmann <mail@paulbrachmann.com>
2020-12-11 12:28:49 +0100

    Add more environment configuration options

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -20,7 +20,7 @@ import { NodeResolver } from "./node.resolver";
       inject: [ConfigService],
       useFactory: async (configService: ConfigService) => ({
         type: "postgres",
-        host: configService.get("DB_HOST", "localhost"),
+        host: configService.get("DB_HOST", "127.0.0.1"),
         port: configService.get<number>("DB_PORT", 5432),
         username: configService.get("DB_USER", "postgres"),
         password: configService.get("DB_PASSWORD", "postgres"),
