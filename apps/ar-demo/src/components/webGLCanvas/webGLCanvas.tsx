7b0df726856e37a6bfa03a8358d5f73a92ba60c8
Paul Brachmann <mail@paulbrachmann.com>
2021-02-21 11:00:22 +0100

    Fix canvas event handling

--- apps/ar-demo/src/components/webGLCanvas/webGLCanvas.tsx
+++ apps/ar-demo/src/components/webGLCanvas/webGLCanvas.tsx
@@ -1,24 +1,36 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 import { WebGLCanvasProps } from "./webGLCanvas.props";
 
-const StyledCanvas = styled.canvas`
-  cursor: crosshair;
+const coverMixin = css`
   position: absolute;
   top: 0;
   left: 0;
   bottom: 0;
   right: 0;
+`;
+
+const StyledCanvas = styled.canvas`
+  ${coverMixin}
+  cursor: crosshair;
 
   &:focus {
     outline: none;
   }
 `;
 
+const StyledDiv = styled.div`
+  ${coverMixin}
+`;
+
 const WebGLCanvas = React.forwardRef<HTMLCanvasElement, WebGLCanvasProps>(
   (props, ref) => {
-    return <StyledCanvas {...props} width={1} height={1} ref={ref} />;
+    return (
+      <StyledDiv>
+        <StyledCanvas {...props} width={1} height={1} ref={ref} />
+      </StyledDiv>
+    );
   },
 );
 
