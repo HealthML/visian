98cd1042e48bbba582352dc34cedb94d1dce79b0
Jonas Kordt <jonas.kordt@student.hpi.de>
2021-02-26 12:32:06 +0100

    Configure raw loader for .glsl files

--- apps/ativ-seminar/webpack.config.js
+++ apps/ativ-seminar/webpack.config.js
@@ -4,18 +4,24 @@ const nrwlConfig = require("@nrwl/react/plugins/webpack");
 module.exports = (config) => {
   nrwlConfig(config);
 
-  config.module.rules.push({
-    test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-    use: [
-      {
-        loader: "file-loader",
-        options: {
-          name: "[name].[ext]",
-          outputPath: "fonts/",
+  config.module.rules.push(
+    {
+      test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+      use: [
+        {
+          loader: "file-loader",
+          options: {
+            name: "[name].[ext]",
+            outputPath: "fonts/",
+          },
         },
-      },
-    ],
-  });
+      ],
+    },
+    {
+      test: /\.glsl$/i,
+      use: "raw-loader",
+    },
+  );
 
   return config;
 };
