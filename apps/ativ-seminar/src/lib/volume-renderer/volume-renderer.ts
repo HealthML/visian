76667c647578a33339dacef6e4be5ff1957c5875
Paul Brachmann <mail@paulbrachmann.com>
2021-03-09 12:20:56 +0100

    Change fly controls

--- apps/ativ-seminar/src/lib/volume-renderer/volume-renderer.ts
+++ apps/ativ-seminar/src/lib/volume-renderer/volume-renderer.ts
@@ -169,7 +169,7 @@ export class VolumeRenderer implements IDisposable {
     this.orbitControls.target.add(this.camera.position);
   };
 
-  private toggleControls = () => {
+  private toggleFly = () => {
     if (this.flyControls.isLocked) {
       this.flyControls.unlock();
     } else {
@@ -178,8 +178,8 @@ export class VolumeRenderer implements IDisposable {
   };
 
   private onKeyDown = (event: KeyboardEvent) => {
-    if (event.key.toLowerCase() === "t") {
-      this.toggleControls();
+    if (event.key === "F") {
+      this.toggleFly();
     }
   };
 }
