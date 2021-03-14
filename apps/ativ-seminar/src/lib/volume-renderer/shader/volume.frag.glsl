f972340171659a538273885c0ea866280b1dc022
Jonas Kordt <jonas.kordt@student.hpi.de>
2021-03-14 13:20:16 +0100

    Add cutaway cone context (wip)

--- apps/ativ-seminar/src/lib/volume-renderer/shader/volume.frag.glsl
+++ apps/ativ-seminar/src/lib/volume-renderer/shader/volume.frag.glsl
@@ -92,8 +92,48 @@ VolumeData getInterpolatedVolumeData(vec3 volumeCoords) {
   return interpolatedData;
 }
 
+/**
+ * Taken from https://www.iquilezles.org/www/articles/distfunctions/distfunctions.htm
+ */
+float sdCone(vec3 p, float angle) {
+    vec2 c = vec2(sin(angle), cos(angle));
+    vec2 q = vec2( length(p.xz), -p.y );
+    float d = length(q-c*max(dot(q,c), 0.0));
+    return d * ((q.x*c.y-q.y*c.x<0.0)?-1.0:1.0);
+}
+
+// TODO: Why does y have to be flipped?
+vec3 normalizedCamera = normalize(vec3(uCameraPosition.x, -uCameraPosition.y, uCameraPosition.z));
+vec3 normalizedConeAxis = vec3(0.0, 1.0, 0.0);
+
+/**
+ * Taken from https://gist.github.com/kevinmoran/b45980723e53edeb8a5a43c49f134724
+ */
+vec3 axis = cross( normalizedCamera, normalizedConeAxis );
+
+float cosA = dot( normalizedCamera, normalizedConeAxis );
+float k = 1.0 / (1.0 + cosA);
+
+mat3 rotation = mat3( 
+  (axis.x * axis.x * k) + cosA,
+  (axis.y * axis.x * k) - axis.z, 
+  (axis.z * axis.x * k) + axis.y,
+  (axis.x * axis.y * k) + axis.z,  
+  (axis.y * axis.y * k) + cosA,      
+  (axis.z * axis.y * k) - axis.x,
+  (axis.x * axis.z * k) - axis.y,  
+  (axis.y * axis.z * k) + axis.x,  
+  (axis.z * axis.z * k) + cosA 
+);
+
+vec3 transformToCutawaySpace(vec3 volumeCoords) {
+  // The cutaway origin should be at the center of the volume.
+  // Thus, we subtract vec3(0.5).
+  return rotation * (volumeCoords - vec3(0.5));
+}
+
 /** The transfer function. */
-vec4 transferFunction(VolumeData data) {
+vec4 transferFunction(VolumeData data, vec3 volumeCoords) {
   // return vec4(data.firstDerivative * 3.0, data.density);
 
   // return vec4(vec3(0.7 * data.density), mix(0.0, 0.02, step(0.1, length(data.firstDerivative))));
@@ -106,13 +146,18 @@ vec4 transferFunction(VolumeData data) {
   //     vec4(vec3(0.5), mix(0.0, 0.015, step(0.1, length(data.firstDerivative))))
   //   : vec4(vec3(1), mix(mix(0.0, 0.015, step(0.1, length(data.firstDerivative))) * 0.2, 0.02, data.focus));
 
-  return mix(vec4(vec3(0.5), mix(0.0, 0.015, step(0.12, length(data.firstDerivative)))), vec4(1.0, 0.0, 0.0, 1.0), data.focus);
+  // return mix(vec4(vec3(0.5), mix(0.0, 0.015, step(0.12, length(data.firstDerivative)))), vec4(1.0, 0.0, 0.0, 1.0), data.focus);
+
+  // TODO: Extract the angle into a uniform.
+  float cone = sdCone(transformToCutawaySpace(volumeCoords), 1.0);
+  float contextFactor = step(0.0, cone);
+  return mix(vec4(data.density * step(0.05, data.density) * contextFactor), vec4(1.0, 0.0, 0.0, 1.0), step(0.1, data.focus));
 
   // return vec4(data.density * step(0.05, data.density));
   // return mix(vec4(0.0), vec4(1.0, 0.0, 0.0, 1.0), step(0.1, data.focus));
 }
 
-// These should be uniforms.
+// TODO: These should be uniforms.
 float ambient = 0.3;
 vec3 lightPosition = vec3(2.0); // In volume coordinates.
 float intensity = 1.0;
@@ -136,8 +181,9 @@ vec4 phong(vec4 volumeColor, VolumeData volumeData, vec3 volumeCoords) {
 
 vec4 getVolumeColor(vec3 volumeCoords) {
   VolumeData volumeData = getInterpolatedVolumeData(volumeCoords);
-  vec4 volumeColor = transferFunction(volumeData);
-  return phong(volumeColor, volumeData, volumeCoords);
+  vec4 volumeColor = transferFunction(volumeData, volumeCoords);
+  return volumeColor;
+  // return phong(volumeColor, volumeData, volumeCoords);
 }
 
 /**
