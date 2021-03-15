bb16732e6766673ab768c7517ed26a5c02c85b8d
Paul Brachmann <mail@paulbrachmann.com>
2021-03-15 19:38:28 +0100

    Implement computeStyleValue utility

--- libs/ui-shared/src/lib/components/slider/slider.tsx
+++ libs/ui-shared/src/lib/components/slider/slider.tsx
@@ -2,18 +2,17 @@ import React, {
   PointerEvent as ReactPointerEvent,
   useCallback,
   useRef,
-  useState,
 } from "react";
 import styled from "styled-components";
 
 import { SliderProps } from ".";
 import {
   color,
+  computeStyleValue,
   lineHeight,
-  parseNumberFromMetric,
-  parseUnitFromMetric,
   scaleMetric,
   size,
+  ThemeProps,
 } from "../../theme";
 import { pointerToSliderValue, useDrag, valueToSliderPos } from "./utils";
 
@@ -49,17 +48,15 @@ const Thumb = styled.div.attrs<ThumbProps>((props) => ({
   style: {
     top: props.isVertical
       ? props.position
-      : `${
-          (parseNumberFromMetric(size("sliderHeight")(props)) -
-            parseNumberFromMetric(size("sliderThumbHeight")(props))) /
-          2
-        }${parseUnitFromMetric(size("sliderThumbHeight")(props))}`,
+      : computeStyleValue<ThemeProps>(
+          [size("sliderHeight"), size("sliderThumbHeight")],
+          (sliderHeight, thumbHeight) => (sliderHeight - thumbHeight) / 2,
+        )(props),
     left: props.isVertical
-      ? `${
-          (parseNumberFromMetric(size("sliderHeight")(props)) -
-            parseNumberFromMetric(size("sliderThumbHeight")(props))) /
-          2
-        }${parseUnitFromMetric(size("sliderThumbHeight")(props))}`
+      ? computeStyleValue<ThemeProps>(
+          [size("sliderHeight"), size("sliderThumbHeight")],
+          (sliderHeight, thumbHeight) => (sliderHeight - thumbHeight) / 2,
+        )(props)
       : props.position,
   },
 }))<ThumbProps>`
--- libs/ui-shared/src/lib/theme/utils.ts
+++ libs/ui-shared/src/lib/theme/utils.ts
@@ -1,3 +1,4 @@
+import string from "*.ttf";
 import { Theme, ThemeProps } from "./theme";
 
 /** Extracts a value from the theme passed to the component. */
@@ -36,8 +37,8 @@ export const zIndex = lookup("zIndices");
  * parseNumberFromMetric("8px");
  */
 export const parseNumberFromMetric = (value: string) => {
-  const match = value.match(/\d+(\.\d+)?/);
-  if (!match) return 0;
+  const match = value.match(/^(\+|-)?\d+(\.\d+)?/);
+  if (!match) return NaN;
   return parseFloat(match[0]);
 };
 
@@ -49,7 +50,7 @@ export const parseNumberFromMetric = (value: string) => {
  * parseUnitFromMetric("8px");
  */
 export const parseUnitFromMetric = (value: string) => {
-  const match = value.match(/([a-z]+)/);
+  const match = value.match(/[a-z]+$/);
   if (!match) return "";
   return match[0];
 };
@@ -59,9 +60,77 @@ export const parseUnitFromMetric = (value: string) => {
  *
  * @param value The metric value.
  * @param factor The factor to scale by.
+ *
  * @example
  * // Returns "16px"
  * scaleMetric("8px", 2);
  */
 export const scaleMetric = (value: string, factor: number) =>
   `${parseNumberFromMetric(value) * factor}${parseUnitFromMetric(value)}`;
+
+export type computationInput<P> =
+  | string
+  | number
+  | ((props: P) => string | number);
+
+/**
+ * Returns a computed value to be used in styling.
+ *
+ * @param inputs A single input or an array of inputs.
+ * @param computeValue A function that computes the resulting styling value
+ * based on the given inputs.
+ *
+ * @example
+ * // Returns "6px"
+ * computeStyleValue(["3px", 2], (value, scale) => value * scale)({});
+ *
+ * @example
+ * // Returns half of `theme.sizes.icon`
+ * computeStyleValue(size("icon"), (iconSize) => iconSize / 2)(props);
+ */
+export const computeStyleValue = <
+  P = { [key: string]: unknown },
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  T extends string | number = any,
+  I extends computationInput<P> | computationInput<P>[] =
+    | computationInput<P>
+    | computationInput<P>[]
+>(
+  inputs: I,
+  computeValue: (...values: T[]) => string | number,
+) => (props: P): string => {
+  // const resolvedValues = Array.isArray(values) ? values : [values];
+  const rawInputs = (Array.isArray(inputs)
+    ? inputs
+    : [inputs]) as computationInput<P>[];
+
+  // Resolve function inputs
+  const resolvedInputs = rawInputs.map((input) => {
+    if (typeof input === "function") {
+      return input(props);
+    }
+    return input;
+  });
+
+  // Resolve string inputs
+  const convertedInputs = resolvedInputs.map((input) => {
+    if (typeof input === "string") {
+      const numericInput = parseNumberFromMetric(input);
+      if (!isNaN(numericInput)) return numericInput;
+    }
+    return input;
+  });
+
+  const result = computeValue(
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    ...(convertedInputs as any),
+  );
+  return typeof result === "string"
+    ? result
+    : `${result}${
+        typeof resolvedInputs[0] === "string" &&
+        resolvedInputs[0].match(/^(\+|-)?\d/)
+          ? parseUnitFromMetric(resolvedInputs[0])
+          : ""
+      }`;
+};
