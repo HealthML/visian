571291112ad4c31a6d51e6f80994d7fb3939fc14
Jonas Kordt <jonas.kordt@student.hpi.de>
2021-04-19 16:12:45 +0200

    Remove old export

--- apps/ativ-seminar/src/lib/volume-renderer/utils/index.ts
+++ apps/ativ-seminar/src/lib/volume-renderer/utils/index.ts
@@ -3,7 +3,6 @@ export * from "./gradient-computer";
 export * from "./histogram";
 export * from "./lao-computer";
 export * from "./resolution-computer";
-export * from "./screen-aligned-quad";
 export * from "./fly-controls";
 export * from "./transfer-function";
 export * from "./lighting-mode";
