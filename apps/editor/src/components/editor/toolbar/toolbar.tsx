c1d5752364ba6276b1dbabf0bdbf778537638e67
Paul Brachmann <mail@paulbrachmann.com>
2021-04-20 13:33:04 +0200

    Fix tool switching while modal is up

--- apps/editor/src/components/editor/toolbar/toolbar.tsx
+++ apps/editor/src/components/editor/toolbar/toolbar.tsx
@@ -51,16 +51,22 @@ export const Toolbar: React.FC = observer(() => {
       ) {
         event.preventDefault();
         event.stopPropagation();
-        setIsModalOpen(!isModalOpen);
+        setIsModalOpen(
+          store?.editor.tools.activeTool !== value || !isModalOpen,
+        );
       }
 
       store?.editor.tools.setActiveTool(value as ToolType);
     },
     [isModalOpen, store],
   );
-  const clearSlice = useCallback(() => {
-    store?.editor.tools.clearSlice();
-  }, [store]);
+  const clearSlice = useCallback(
+    (_value: undefined, event: React.PointerEvent) => {
+      if (event.button !== PointerButton.LMB) return;
+      store?.editor.tools.clearSlice();
+    },
+    [store],
+  );
 
   return (
     <StyledToolbar>
@@ -70,16 +76,18 @@ export const Toolbar: React.FC = observer(() => {
         activeTool={activeTool}
         value={ToolType.Navigate}
         onPress={setActiveTool}
+        onContextMenu={preventDefault}
       />
       <Tool
         icon="crosshair"
         tooltipTx="crosshair-tool"
         activeTool={activeTool}
         value={ToolType.Crosshair}
-        onPress={setActiveTool}
         isDisabled={
           store?.editor.image && store.editor.image.dimensionality < 3
         }
+        onPress={setActiveTool}
+        onContextMenu={preventDefault}
       />
       <Tool
         icon="pixelBrush"
@@ -111,7 +119,12 @@ export const Toolbar: React.FC = observer(() => {
         onPress={setActiveTool}
         onContextMenu={preventDefault}
       />
-      <Tool icon="trash" tooltipTx="clear-slice" onPress={clearSlice} />
+      <Tool
+        icon="trash"
+        tooltipTx="clear-slice"
+        onPress={clearSlice}
+        onContextMenu={preventDefault}
+      />
       <Modal
         style={modalPosition}
         isOpen={
