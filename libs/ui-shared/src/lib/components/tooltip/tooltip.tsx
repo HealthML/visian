bd80f1769b8766f4f3c1ccb7c22d40ab74b7bc4a
maximilianlindner <53479501+maximilianlindner@users.noreply.github.com>
2021-04-20 18:34:22 +0200

    Resize Tooltips

--- libs/ui-shared/src/lib/components/tooltip/tooltip.tsx
+++ libs/ui-shared/src/lib/components/tooltip/tooltip.tsx
@@ -12,8 +12,8 @@ const TooltipContainer = styled(Sheet).withConfig({
   shouldForwardProp: (prop) => prop.toString() !== "isActive",
 })<Pick<TooltipProps, "baseZIndex">>`
   box-sizing: border-box;
-  height: 16px;
-  border-radius: 10px;
+  height: 24px;
+  border-radius: 12px;
   padding: 0 14px;
   z-index: ${(props) =>
     (props.baseZIndex === undefined
@@ -22,8 +22,8 @@ const TooltipContainer = styled(Sheet).withConfig({
 `;
 
 const TooltipLabel = styled(Text)`
-  font-size: 9px;
-  line-height: 9px;
+  font-size: 12px;
+  line-height: 12px;
   white-space: nowrap;
   overflow: hidden;
   text-overflow: ellipsis;
