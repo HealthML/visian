43f2dc339b23d00b3c3672f6c5536cac362765fc
Paul Brachmann <mail@paulbrachmann.com>
2021-04-22 18:54:07 +0200

    Implement automatic tool switching

--- libs/ui-shared/src/index.ts
+++ libs/ui-shared/src/index.ts
@@ -1,6 +1,7 @@
 export * from "./lib/components";
 export * from "./lib/event-handling";
 export * from "./lib/i18n";
+export * from "./lib/platform-detection";
 export * from "./lib/storage-backend";
 export * from "./lib/tab";
 export * from "./lib/theme";
