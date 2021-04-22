2e2eb852fac28202d5df658f2f9f2a0d90eef4f3
Paul Brachmann <mail@paulbrachmann.com>
2021-04-22 22:04:49 +0200

    Remove unnecessary UndoRedo prefixes

--- apps/editor/src/components/editor/undo-redo-buttons/undo-redo-buttons.tsx
+++ apps/editor/src/components/editor/undo-redo-buttons/undo-redo-buttons.tsx
@@ -5,16 +5,16 @@ import styled from "styled-components";
 
 import { useStore } from "../../../app/root-store";
 
-const UndoRedoContainer = styled.div`
+const Container = styled.div`
   position: relative;
 `;
-const UndoRedoWrapper = styled.div`
+const Wrapper = styled.div`
   display: flex;
   left: 0;
   position: absolute;
   top: 0;
 `;
-const UndoRedoButton = styled(SquareButton)`
+const StyledButton = styled(SquareButton)`
   margin-right: 8px;
 `;
 
@@ -22,21 +22,21 @@ export const UndoRedoButtons = observer(() => {
   const store = useStore();
 
   return (
-    <UndoRedoContainer>
-      <UndoRedoWrapper>
-        <UndoRedoButton
+    <Container>
+      <Wrapper>
+        <StyledButton
           icon="undo"
           isActive={false}
           isDisabled={!store?.editor.undoRedo.isUndoAvailable}
           onPointerDown={store?.editor.undoRedo.undo}
         />
-        <UndoRedoButton
+        <StyledButton
           icon="redo"
           isActive={false}
           isDisabled={!store?.editor.undoRedo.isRedoAvailable}
           onPointerDown={store?.editor.undoRedo.redo}
         />
-      </UndoRedoWrapper>
-    </UndoRedoContainer>
+      </Wrapper>
+    </Container>
   );
 });
