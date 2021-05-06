1c7f4da1e9ed9c59c98d35391a4ef48d9e94052b
Jonas Kordt <jonas.kordt@student.hpi.de>
2021-05-06 15:59:15 +0200

    Fix annotation not working on some slices

--- apps/editor/src/models/editor/rendered-image/edit-rendering/shaders/voxel.vert.glsl
+++ apps/editor/src/models/editor/rendered-image/edit-rendering/shaders/voxel.vert.glsl
@@ -13,6 +13,12 @@ void main() {
     floor(position.z / uAtlasGrid.x)
   );
 
+  // TODO: Why does this case even happpen?
+  if (delta.x == uAtlasGrid.x) {
+    delta.x = 0.0;
+    delta.y += 1.0;
+  }
+
   vec2 atlasPosition = delta * uVoxelCount.xy + position.xy;
 
   gl_PointSize = 1.0;
