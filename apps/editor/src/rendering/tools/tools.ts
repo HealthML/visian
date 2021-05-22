618d45c33b74b16cc74d161382ede37f7a5ae507
Jonas Kordt <jonas.kordt@student.hpi.de>
2021-05-22 16:02:17 +0200

    Fix outline tool overriding smart brush edits

--- apps/editor/src/rendering/tools/tools.ts
+++ apps/editor/src/rendering/tools/tools.ts
@@ -180,7 +180,7 @@ export class EditorTools implements ISerializable<EditorToolsSnapshot> {
     if (this.isDrawing) return;
 
     // Temporary fix so that brush & eraser don't overwrite smart brush edits.
-    if ([ToolType.Brush, ToolType.Eraser].includes(tool)) {
+    if ([ToolType.Brush, ToolType.Eraser, ToolType.Outline].includes(tool)) {
       this.toolRenderer.readCurrentSlice();
     }
 
