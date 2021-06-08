daa451bb64e52d4ef021f0a754ed5d24369a1fca
Paul Brachmann <mail@paulbrachmann.com>
2021-06-08 14:42:17 +0200

    Add FileParameter

---
+++ libs/ui-shared/src/lib/components/params/file-param.tsx
@@ -0,0 +1,85 @@
+import React, { useCallback } from "react";
+import styled from "styled-components";
+
+import { fontSize } from "../../theme";
+import { IFileParameter } from "../../types";
+import { InvisibleButton } from "../button";
+import { sheetMixin } from "../sheet";
+import { InputLabel, Text } from "../text";
+import { ListPositionProps } from "./types";
+
+const InvisibleFileInput = styled.input.attrs(() => ({ type: "file" }))`
+  display: none;
+`;
+
+const Container = styled.label`
+  ${sheetMixin}
+  align-items: center;
+  box-sizing: border-box;
+  border-radius: 12px;
+  cursor: pointer;
+  display: flex;
+  height: 24px;
+  margin-bottom: 10px;
+  padding: 0 14px;
+  width: 100%;
+`;
+
+const FileInputText = styled(Text)<{ isFileLoaded?: boolean }>`
+  flex: 1;
+  font-size: ${fontSize("small")};
+  line-height: 10px;
+  text-align: ${(props) => (props.isFileLoaded ? "left" : "center")};
+`;
+
+// TODO: Replace `trash` with an x icon
+const XButton = styled(InvisibleButton).attrs(() => ({ icon: "trash" }))`
+  height: 20px;
+  width: 20px;
+`;
+
+export type FileParamProps = IFileParameter &
+  ListPositionProps &
+  Omit<React.HTMLAttributes<HTMLInputElement>, "defaultValue" | "onChange">;
+
+// TODO: In the future, we should probably offer a more flexible pop up color
+// picker that allows user to specify fully custom colors
+export const FileParam: React.FC<Partial<FileParamProps>> = ({
+  isFirst,
+  labelTx,
+  label,
+  value,
+  setValue,
+
+  defaultValue,
+  onBeforeValueChange,
+  kind,
+  name,
+  ...rest
+}) => {
+  const setFile = useCallback(
+    (event?: React.ChangeEvent<HTMLInputElement>) => {
+      if (setValue) setValue(event?.target.files?.[0]);
+    },
+    [setValue],
+  );
+
+  const clearFile = useCallback(() => {
+    if (setValue) setValue(undefined);
+  }, [setValue]);
+
+  return (
+    <>
+      {(labelTx || label) && <InputLabel tx={labelTx} text={label} />}
+      <Container>
+        <InvisibleFileInput {...rest} onChange={setFile} />
+        <FileInputText
+          isFileLoaded={Boolean(value)}
+          tx={value ? undefined : "upload"}
+          text={value?.name}
+        />
+        {Boolean(value) && <XButton onPointerDown={clearFile} />}
+      </Container>
+    </>
+  );
+};
