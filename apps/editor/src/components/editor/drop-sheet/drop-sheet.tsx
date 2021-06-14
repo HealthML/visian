9cf56b0e0d9bf2dce6b17a7bbfd0da172398d501
Clara Uktar <clara.uktar@student.hpi.uni-potsdam.de>
2021-06-14 03:25:00 +0200

    Exclude hidden system files from import

--- apps/editor/src/components/editor/drop-sheet/drop-sheet.tsx
+++ apps/editor/src/components/editor/drop-sheet/drop-sheet.tsx
@@ -55,6 +55,8 @@ export const DropSheet: React.FC<DropSheetProps> = observer(
 
     const importSingleFile = useCallback(
       async (file: File) => {
+        // Exclude hidden system files from import
+        if (file.name.startsWith(".")) return;
         if (await isFileAnnotation(file)) {
           await store?.editor.activeDocument?.importAnnotation(file);
         } else {
@@ -83,33 +85,19 @@ export const DropSheet: React.FC<DropSheetProps> = observer(
       [importSingleFile, store?.editor.activeDocument],
     );
 
-    const getFile = useCallback(
+    const importFileEntry = useCallback(
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      async (fileEntry: any) => {
-        try {
-          return await new Promise<File>((resolve, reject) =>
-            fileEntry.file(resolve, reject),
-          );
-        } catch (error) {
-          store?.setError({
-            titleTx: "import-error",
-            descriptionTx: error.message,
+      async (entry: any) =>
+        new Promise<void>((resolve) => {
+          entry.file(async (file: File) => {
+            await importSingleFile(file);
+            resolve();
           });
-        }
-      },
-      [store],
+        }),
+      [importSingleFile],
     );
 
-    const importFileItem = useCallback(
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      async (entry: any) => {
-        const file = await getFile(entry);
-        if (file) await importSingleFile(file);
-      },
-      [getFile, importSingleFile],
-    );
-
-    const importDirectoryItem = useCallback(
+    const importDirectoryEntry = useCallback(
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
       async (entry: any) => {
         const dirFiles: File[] = [];
@@ -136,7 +124,7 @@ export const DropSheet: React.FC<DropSheetProps> = observer(
               }),
             );
           } else {
-            promises.push(importDirectoryItem(entries[i]));
+            promises.push(importDirectoryEntry(entries[i]));
           }
         }
         await Promise.all(promises);
@@ -161,63 +149,13 @@ export const DropSheet: React.FC<DropSheetProps> = observer(
               const entry = item?.webkitGetAsEntry();
               if (entry) {
                 if (entry.isDirectory) {
-                  promises.push(importDirectoryItem(entry));
+                  promises.push(importDirectoryEntry(entry));
                 } else {
-                  promises.push(importFileItem(entry));
+                  promises.push(importFileEntry(entry));
                 }
               }
             }
             await Promise.all(promises);
-            // const item = event.dataTransfer.items[0];
-            // // console.log(event.dataTransfer.items);
-            // const entry = item?.webkitGetAsEntry();
-            // const dirFiles: File[] = [];
-            // let dirName: string | undefined;
-            // if (entry && entry.isDirectory) {
-            //   dirName = entry.name;
-            //   const dirReader = entry.createReader();
-            //   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            //   const entries = await new Promise<any[]>((resolve) => {
-            //     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            //     dirReader.readEntries((result: any[]) => {
-            //       resolve(result);
-            //     });
-            //   });
-
-            //   const promises: Promise<void>[] = [];
-            //   const { length } = entries;
-            //   for (let i = 0; i < length; i++) {
-            //     if (entries[i].isFile) {
-            //       promises.push(
-            //         new Promise((resolve) => {
-            //           entries[i].file((file: File) => {
-            //             dirFiles.push(file);
-            //             resolve();
-            //           });
-            //         }),
-            //       );
-            //     }
-            //   }
-            //   await Promise.all(promises);
-            // }
-
-            // if (dirFiles.length) {
-            //   if (await isFileAnnotation(dirFiles[0])) {
-            //     await store?.editor.activeDocument?.importAnnotation(
-            //       dirFiles,
-            //       dirName,
-            //     );
-            //   } else {
-            //     await store?.editor.activeDocument?.importImage(
-            //       dirFiles,
-            //       dirName,
-            //     );
-            //   }
-            // } else if (await isFileAnnotation(files[0])) {
-            //   await store?.editor.activeDocument?.importAnnotation(files[0]);
-            // } else {
-            //   await store?.editor.activeDocument?.importImage(files[0]);
-            // }
             store?.setError();
           } catch (error) {
             store?.setError({
@@ -230,7 +168,7 @@ export const DropSheet: React.FC<DropSheetProps> = observer(
           onDropCompleted();
         })();
       },
-      [importDirectoryItem, importFileItem, onDropCompleted, store],
+      [importDirectoryEntry, importFileEntry, onDropCompleted, store],
     );
 
     const modalRootRef = useModalRoot();
