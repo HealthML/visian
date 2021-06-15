fe9d70dd00d1d57a548323841a9337ea46706f1c
Jonas Kordt <jonas.kordt@student.hpi.de>
2021-06-15 13:56:02 +0200

    Use quadratic opacity slider when in 3D mode

--- apps/editor/src/components/editor/layer-settings/layer-settings.tsx
+++ apps/editor/src/components/editor/layer-settings/layer-settings.tsx
@@ -1,18 +1,28 @@
 import { ColorParam, Modal, NumberParam } from "@visian/ui-shared";
 import { observer } from "mobx-react-lite";
 import React from "react";
+import { useStore } from "../../../app/root-store";
 
 import { LayerSettingsProps } from "./layer-settings.props";
 
 export const LayerSettings = observer<LayerSettingsProps>(
-  ({ layer, ...rest }) => (
-    <Modal {...rest} labelTx="layer-settings" onReset={layer.resetSettings}>
-      <NumberParam
-        labelTx="opacity"
-        value={layer.opacity}
-        setValue={layer.setOpacity}
-      />
-      <ColorParam value={layer.color} setValue={layer.setColor} />
-    </Modal>
-  ),
+  ({ layer, ...rest }) => {
+    const store = useStore();
+
+    return (
+      <Modal {...rest} labelTx="layer-settings" onReset={layer.resetSettings}>
+        <NumberParam
+          labelTx="opacity"
+          value={layer.opacity}
+          setValue={layer.setOpacity}
+          scaleType={
+            store?.editor.activeDocument?.viewSettings.viewMode === "3D"
+              ? "quadratic"
+              : "linear"
+          }
+        />
+        <ColorParam value={layer.color} setValue={layer.setColor} />
+      </Modal>
+    );
+  },
 );
