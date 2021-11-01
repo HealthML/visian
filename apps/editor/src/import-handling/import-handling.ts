4b3e5271bcb2ddc031317fbd21995adca742fc77
Clara Uktar <clara.uktar@student.hpi.uni-potsdam.de>
2021-11-01 21:44:21 +0100

    Fix new document with drag & drop in Safari

--- apps/editor/src/import-handling/import-handling.ts
+++ apps/editor/src/import-handling/import-handling.ts
@@ -2,33 +2,41 @@ import { ImageMismatchError } from "@visian/utils";
 import { RootStore } from "../models";
 
 const importFilesByType = async (
-  files: FileList | DataTransferItemList,
+  files: FileList | FileSystemEntry[],
   store: RootStore,
 ) => {
-  if (files instanceof DataTransferItemList) {
-    const entries: FileSystemEntry[] = [];
-    for (let fileIndex = 0; fileIndex < files.length; fileIndex++) {
-      const item = files[fileIndex];
-      const entry = item?.webkitGetAsEntry();
-      if (entry) entries.push(entry);
-    }
-    await store.editor.activeDocument?.importFileSystemEntries(entries);
-  } else {
+  if (files instanceof FileList) {
     await store.editor.activeDocument?.importFiles(Array.from(files));
+  } else {
+    await store.editor.activeDocument?.importFileSystemEntries(files);
   }
 };
 
+const getFileSystemEntriesFromDataTransfer = (items: DataTransferItemList) => {
+  const entries: FileSystemEntry[] = [];
+  for (let itemIndex = 0; itemIndex < items.length; itemIndex++) {
+    const item = items[itemIndex];
+    const entry = item?.webkitGetAsEntry();
+    if (entry) entries.push(entry);
+  }
+  return entries;
+};
+
 const handleImportWithErrors = async (
-  files: FileList | DataTransferItemList,
+  files: FileList | DataTransferItemList | FileSystemEntry[],
   store: RootStore,
   shouldRetry = false,
 ) => {
+  const filesForImport =
+    files instanceof DataTransferItemList
+      ? getFileSystemEntriesFromDataTransfer(files)
+      : files;
   try {
-    await importFilesByType(files, store);
+    await importFilesByType(filesForImport, store);
   } catch (error) {
     if (shouldRetry && error instanceof ImageMismatchError) {
       if (store.editor.newDocument()) {
-        await handleImportWithErrors(files, store, false);
+        await handleImportWithErrors(filesForImport, store, false);
       } else {
         store.setError({
           titleTx: "import-error",
