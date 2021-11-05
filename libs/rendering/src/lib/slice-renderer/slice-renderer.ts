1e613538ab3a4f43852fd011d1a8fcc8db4d0fa2
Jonas Kordt <jonas.kordt@student.hpi.de>
2021-11-06 00:10:36 +0100

    Improve rendered sheet rounded corners

--- libs/rendering/src/lib/slice-renderer/slice-renderer.ts
+++ libs/rendering/src/lib/slice-renderer/slice-renderer.ts
@@ -67,23 +67,42 @@ export class SliceRenderer implements IDisposable, ISliceRenderer {
     const height = 1;
     const x = -width / 2;
     const y = -height / 2;
-    const radius = 0.02;
+    const radius = 0.018;
 
     const shape = new THREE.Shape();
     shape.moveTo(x, y + radius);
     shape.lineTo(x, y + height - radius);
-    shape.quadraticCurveTo(x, y + height, x + radius, y + height);
-    shape.lineTo(x + width - radius, y + height);
-    shape.quadraticCurveTo(
-      x + width,
-      y + height,
-      x + width,
+    shape.absarc(
+      x + radius,
       y + height - radius,
+      radius,
+      Math.PI,
+      0.5 * Math.PI,
+      true,
     );
+    shape.moveTo(x + radius, y + height);
+    shape.lineTo(x + width - radius, y + height);
+    shape.absarc(
+      x + width - radius,
+      y + height - radius,
+      radius,
+      0.5 * Math.PI,
+      0,
+      true,
+    );
+    shape.moveTo(x + width, y + height - radius);
     shape.lineTo(x + width, y + radius);
-    shape.quadraticCurveTo(x + width, y, x + width - radius, y);
+    shape.absarc(
+      x + width - radius,
+      y + radius,
+      radius,
+      0,
+      -0.5 * Math.PI,
+      true,
+    );
+    shape.moveTo(x + width - radius, y);
     shape.lineTo(x + radius, y);
-    shape.quadraticCurveTo(x, y, x, y + radius);
+    shape.absarc(x + radius, y + radius, radius, -0.5 * Math.PI, Math.PI, true);
 
     const geometry = new THREE.ShapeBufferGeometry(shape);
     (geometry.attributes.uv as THREE.BufferAttribute).copyArray(
