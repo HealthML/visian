8f8c44bbace020f755cd48d87fe37e558715ef4e
Jonas Kordt <jonas.kordt@student.hpi.de>
2021-11-13 18:44:44 +0100

    Remove debug GUI code

--- libs/rendering/src/lib/rendered-sheet/rendered-sheet.ts
+++ libs/rendering/src/lib/rendered-sheet/rendered-sheet.ts
@@ -3,8 +3,6 @@ import * as THREE from "three";
 import { BlurMaterial } from "./blur-material";
 import { RenderedSheetGeometry } from "./rendered-sheet-geometry";
 
-// import { GUI } from "three/examples/jsm/libs/dat.gui.module.js";
-
 export class RenderedSheet extends THREE.Mesh {
   private sharedGeometry: RenderedSheetGeometry;
 
@@ -12,7 +10,7 @@ export class RenderedSheet extends THREE.Mesh {
     const params = {
       transmission: 1,
       roughness: 0.75,
-      ior: 1.47,
+      ior: 1.49,
       thickness: 0,
       backgroundOpacity: 0.2,
       noiseOpacity: 1,
@@ -60,39 +58,5 @@ export class RenderedSheet extends THREE.Mesh {
     );
     outline.position.z = 1;
     this.add(outline);
-
-    // const gui = new GUI();
-    // gui.add(params, "transmission", 0, 1, 0.01).onChange(() => {
-    //   (this.material as BlurMaterial).transmission = params.transmission;
-    //   editor.sliceRenderer?.lazyRender();
-    // });
-    // gui.add(params, "roughness", 0, 1, 0.01).onChange(() => {
-    //   (this.material as BlurMaterial).roughness = params.roughness;
-    //   editor.sliceRenderer?.lazyRender();
-    // });
-    // gui.add(params, "ior", 1, 2.333, 0.01).onChange(() => {
-    //   (this.material as BlurMaterial).ior = params.ior;
-    //   editor.sliceRenderer?.lazyRender();
-    // });
-    // gui.add(params, "thickness", 0, 0.2, 0.005).onChange(() => {
-    //   (this.material as BlurMaterial).thickness = params.thickness;
-    //   editor.sliceRenderer?.lazyRender();
-    // });
-    // gui.add(params, "backgroundOpacity", 0, 1, 0.01).onChange(() => {
-    //   (backgroundLayer.material as THREE.MeshBasicMaterial).opacity =
-    //     params.backgroundOpacity;
-    //   editor.sliceRenderer?.lazyRender();
-    // });
-    // gui.add(params, "noiseOpacity", 0, 1, 0.01).onChange(() => {
-    //   (noiseLayer.material as THREE.MeshBasicMaterial).opacity =
-    //     params.noiseOpacity;
-    //   editor.sliceRenderer?.lazyRender();
-    // });
-    // gui.add(params, "outlineOpacity", 0, 1, 0.01).onChange(() => {
-    //   (outline.material as THREE.LineBasicMaterial).opacity =
-    //     params.outlineOpacity;
-    //   editor.sliceRenderer?.lazyRender();
-    // });
-    // gui.open();
   }
 }
