dab098107006a894abd4af18f1f0bfef5f21d3e7
Luca_Stngn <luca@steingen.net>
2022-02-04 18:09:34 +0100

    Update upload-screen.tsx

--- apps/editor/src/screens/upload-screen.tsx
+++ apps/editor/src/screens/upload-screen.tsx
@@ -150,7 +150,7 @@ export const UploadScreen = observer(() => {
         }
 
         // Calculate approximate time from upload to confirmation E-Mail:
-        setApproxBulkTime((30 + zips.length * (26 / 60)).toFixed(0));
+        setApproxBulkTime((3 + zips.length * (26 / 60)).toFixed(0));
         store?.setProgress(); // Turn off ProgressBar
         setShowProgressPopUp(true);
 
