6e205bf98cbba0a03b4b5f687fc6eb11d399e2da
Luca_Stngn <luca@steingen.net>
2022-02-27 12:14:45 +0100

    Update to new endpoint

--- apps/editor/src/constants/floy.ts
+++ apps/editor/src/constants/floy.ts
@@ -2,7 +2,7 @@
 export const IS_FLOY_DEMO = true;
 export const FLOY_HOME = "https://floy.com";
 export const FLOY_INFERENCE_ENDPOINTS = [
-  "https://valohai.cloud/floy/mr-bone/web-app/segmentation/floy-ai",
+  "https://valohai.cloud/floy/demo-floy-com/demo-floy-com/floy-vision/floy-ai",
 ];
 export const FLOY_API_ROOT =
   process.env.FLOY_API_ROOT || "https://demo-api.floy.com";
