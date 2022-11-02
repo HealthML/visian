58444ce614edd3667dfbdf254019543aa19c075c
konrad-gerlach <konrad.gerlach_dev@gmx.de>
2022-11-02 10:48:07 +0100

    pre annotation is received and downloaded in two different ways and can be loaded as image (not Annotation!)

--- apps/editor/src/components/editor/ui-overlay/ui-overlay.tsx
+++ apps/editor/src/components/editor/ui-overlay/ui-overlay.tsx
@@ -287,9 +287,25 @@ export const UIOverlay = observer<UIOverlayProps>(
                         return response.blob();
                       })
                       .then(function (blob) {
-                        //saveAs(blob, "Test_" + fileName);
-                        var file = new File([blob], "Annotation_" + fileName);
+                        saveAs(blob, "Test_" + fileName);
+                        let file = new File([blob], "Annotation_" + fileName, {
+                          type: blob.type,
+                          lastModified: Date.now(),
+                        });
                         console.log(file);
+                        console.log(file);
+                        console.log(blob);
+
+                        /* hotfixed data download
+                        var url = window.URL || window.webkitURL;
+                        let link = url.createObjectURL(blob);
+                        var a = document.createElement("a");
+                        a.setAttribute("download", fileName);
+                        a.setAttribute("href", link);
+                        document.body.appendChild(a);
+                        a.click();
+                        document.body.removeChild(a);*/
+                        //FileSaver.saveAs(blob, "Annotation_" + fileName);
                         store?.editor.activeDocument?.importFiles(
                           file,
                           "Annotation_" + fileName,
