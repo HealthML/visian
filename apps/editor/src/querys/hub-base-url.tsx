ae9adc438daca1241271af5aedac9e2a5172a6fc
Janis Wehen <janiswehen@gmail.com>
2023-01-25 01:56:34 +0100

    fix: linter

--- apps/editor/src/querys/hub-base-url.tsx
+++ apps/editor/src/querys/hub-base-url.tsx
@@ -1,15 +1,19 @@
 const formatUrl = (url: string | null | undefined) => {
-    if (!url || url == '') {
-        return url;
-    }
-    if (!url.startsWith("http://") && !url.startsWith("https://")) {
-        url = "http://" + url;
-    }
-    if (!url.endsWith("/")) {
-        url = url + "/";
-    }
+  if (!url || url === "") {
     return url;
-}
+  }
+  let formattedUrl = url;
+  if (
+    !formattedUrl.startsWith("http://") &&
+    !formattedUrl.startsWith("https://")
+  ) {
+    formattedUrl = `http://${formattedUrl}`;
+  }
+  if (!formattedUrl.endsWith("/")) {
+    formattedUrl = `${formattedUrl}/`;
+  }
+  return formattedUrl;
+};
 
 export const hubBaseUrl = formatUrl(process.env.NX_ANNOTATION_SERVICE_HUB_URL);
 
