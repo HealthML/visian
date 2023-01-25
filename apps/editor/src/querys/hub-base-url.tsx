5383579d1ca2eab7acb1cd5e7efa90f22175fdcd
Janis Wehen <janiswehen@gmail.com>
2023-01-25 01:23:37 +0100

    feat: ensure hubBaseUrl has correct format

--- apps/editor/src/querys/hub-base-url.tsx
+++ apps/editor/src/querys/hub-base-url.tsx
@@ -1,3 +1,16 @@
-export const hubBaseUrl = process.env.NX_ANNOTATION_SERVICE_HUB_URL;
+const formatUrl = (url: string | null | undefined) => {
+    if (!url || url == '') {
+        return url;
+    }
+    if (!url.startsWith("http://") && !url.startsWith("https://")) {
+        url = "http://" + url;
+    }
+    if (!url.endsWith("/")) {
+        url = url + "/";
+    }
+    return url;
+}
+
+export const hubBaseUrl = formatUrl(process.env.NX_ANNOTATION_SERVICE_HUB_URL);
 
 export default hubBaseUrl;
