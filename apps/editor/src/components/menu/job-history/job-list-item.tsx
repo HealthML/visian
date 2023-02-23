e78bd0907d149b9bd12d02a96eb49429cc4988aa
Tim Riedel <timriedel00@gmail.com>
2023-02-23 11:35:27 +0100

    feat: add job properties to jobs history

--- apps/editor/src/components/menu/job-history/job-list-item.tsx
+++ apps/editor/src/components/menu/job-history/job-list-item.tsx
@@ -2,6 +2,7 @@ import { ListItem, Text } from "@visian/ui-shared";
 import styled from "styled-components";
 
 import { Job } from "../../../types";
+import { getDisplayDate } from "../util/display-date";
 
 const Spacer = styled.div`
   width: 10px;
@@ -15,8 +16,14 @@ export const JobListItem = ({ job }: { job: Job }) => (
   <ListItem>
     <Text>{job.modelName}</Text>
     <Spacer />
-    <Text>{job.modelVersion}</Text>
-    <ExpandedSpacer />
+    <Text>v{job.modelVersion}</Text>
+    <Spacer />
+    <Text>{job.startedAt ? getDisplayDate(new Date(job.startedAt)) : ""}</Text>
+    <Spacer />
+    <Text>
+      {job.finishedAt ? getDisplayDate(new Date(job.finishedAt)) : ""}
+    </Text>
+    <Spacer />
     <Text>{job.status}</Text>
   </ListItem>
 );
---
+++ apps/editor/src/components/menu/util/display-date.tsx
@@ -0,0 +1,25 @@
+/**
+ * Converts a date to a string for either german or english format.
+ * @param date The date to be converted.
+ * @returns The date as a string in german format if the timezone is Europe/Berlin, otherwise in english format.
+ */
+export function getDisplayDate(date: Date): string {
+  if (Intl.DateTimeFormat().resolvedOptions().timeZone == "Europe/Berlin") {
+    return date.toLocaleDateString("de-DE", {
+      year: "numeric",
+      month: "numeric",
+      day: "numeric",
+      hour: "2-digit",
+      hour12: false,
+      minute: "2-digit",
+    });
+  }
+  return date.toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "numeric",
+    day: "numeric",
+    hour: "2-digit",
+    hour12: true,
+    minute: "2-digit",
+  });
+}
