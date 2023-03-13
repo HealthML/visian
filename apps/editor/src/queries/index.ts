ce3c69023c4ed4de68e25f78b3cf2fc70c9f8307
johannaschlimme <johanna.schlimme@gmx.de>
2023-03-13 17:40:05 +0100

    add error handling to project datasets screen

--- apps/editor/src/queries/index.ts
+++ apps/editor/src/queries/index.ts
@@ -1,5 +1,6 @@
 export * from "./use-annotations-by";
 export * from "./use-dataset";
+export * from "./use-datasets-by";
 export * from "./use-images-by";
 export * from "./use-jobs";
 export * from "./use-ml-models";
--- apps/editor/src/screens/project-datasets-screen.tsx
+++ apps/editor/src/screens/project-datasets-screen.tsx
@@ -1,15 +1,18 @@
-import { Screen, useTranslation } from "@visian/ui-shared";
+import { Screen, Text, useTranslation } from "@visian/ui-shared";
 import { observer } from "mobx-react-lite";
 import React from "react";
 import { useParams } from "react-router-dom";
 
 import { DatasetsGrid } from "../components/menu/datasets-grid";
-import UIOverlayMenu from "../components/menu/ui-overlay-menu/ui-overlay-menu";
-import { useProject } from "../queries";
+import { UIOverlayMenu } from "../components/menu/ui-overlay-menu";
+import { useDatasetsBy, useProject } from "../queries";
 
 export const ProjectDatasetsScreen: React.FC = observer(() => {
   const projectId = useParams().projectId || "";
-  const { project, isErrorProject, isLoadingProject } = useProject(projectId);
+  const { project, projectError, isErrorProject, isLoadingProject } =
+    useProject(projectId);
+  const { datasets, datasetsError, isErrorDatasets, isLoadingDatasets } =
+    useDatasetsBy(projectId);
   const { t: translate } = useTranslation();
 
   return (
@@ -29,7 +32,23 @@ export const ProjectDatasetsScreen: React.FC = observer(() => {
         editButton
         projectViewSwitch
         defaultSwitchSelection="datasets"
-        main={projectId && <DatasetsGrid projectId={projectId} />}
+        main={
+          isLoadingProject || isLoadingDatasets ? (
+            <Text>{translate("datasets-loading")}</Text>
+          ) : isErrorProject ? (
+            <Text>{`${translate("error")} ${
+              projectError?.response?.statusText
+            } (${projectError?.response?.status})`}</Text>
+          ) : isErrorDatasets ? (
+            <Text>{`${translate("datasets-loading-error")} ${
+              datasetsError?.response?.statusText
+            } (${datasetsError?.response?.status})`}</Text>
+          ) : datasets && datasets.length > 0 ? (
+            <DatasetsGrid projectId={projectId} />
+          ) : (
+            <Text>{translate("no-datasets-available")}</Text>
+          )
+        }
       />
     </Screen>
   );
