3b883336ee05709fb2cb7c14f21e6d0836390eb2
Tim Riedel <timriedel00@gmail.com>
2023-03-15 17:43:50 +0100

    feat: display images and annotation status on job details popup

--- apps/editor/src/components/menu/job-history/job-details-popup/job-details-popup.tsx
+++ apps/editor/src/components/menu/job-history/job-details-popup/job-details-popup.tsx
@@ -1,21 +1,28 @@
 import {
-  SectionHeader,
+  color as getColor,
   List,
   ListItem,
   PopUp,
+  SectionHeader,
+  Spacer,
   Text,
+  Theme,
   useTranslation,
 } from "@visian/ui-shared";
 import { observer } from "mobx-react-lite";
+import { useCallback } from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
+
 import { useAnnotationsByJob } from "../../../../queries";
+import useImagesByJob from "../../../../queries/use-images-by-jobs";
 import { editorPath } from "../../util";
 import { JobDetailsPopUpProps } from "./job-details-popup.props";
 
 const StyledPopUp = styled(PopUp)`
   align-items: left;
   width: 45vw;
+  height: 60vh;
 `;
 
 const ClickableListItem = styled(ListItem)`
@@ -24,17 +31,42 @@ const ClickableListItem = styled(ListItem)`
   }
 `;
 
+const ScrollableList = styled(List)`
+  overflow-y: auto;
+`;
+
+const AnnotationStatus = styled.div<{ color: string; borderColor: string }>`
+  width: 1em;
+  height: 1em;
+  border-radius: 50%;
+  background-color: ${(props) =>
+    getColor(props.color as keyof Theme["colors"])};
+  border-color: ${(props) =>
+    getColor(props.borderColor as keyof Theme["colors"])};
+`;
+
 export const JobDetailsPopUp = observer<JobDetailsPopUpProps>(
   ({ job, isOpen, onClose }) => {
-    const {
-      annotations,
-      annotationsError,
-      isErrorAnnotations,
-      isLoadingAnnotations,
-    } = useAnnotationsByJob(job.id);
+    const { annotations, annotationsError, isErrorAnnotations } =
+      useAnnotationsByJob(job.id);
+
+    const { images, imagesError, isErrorImages, isLoadingImages } =
+      useImagesByJob(job.id);
 
     const { t } = useTranslation();
     const navigate = useNavigate();
+
+    const imagesWithAnnotations = annotations?.map(
+      (annotation) => annotation.image,
+    );
+
+    const findAnnotationId = useCallback(
+      (imageId: string) => {
+        const annotation = annotations?.find((anno) => anno.image === imageId);
+        return annotation?.id;
+      },
+      [annotations],
+    );
 
     return (
       <StyledPopUp
@@ -43,25 +75,37 @@ export const JobDetailsPopUp = observer<JobDetailsPopUpProps>(
         dismiss={onClose}
         shouldDismissOnOutsidePress
       >
-        <SectionHeader tx={"annotations-generated"} />
+        <SectionHeader tx="job-selected-images" />
+        {isErrorImages && (
+          <Text>{`${t("images-loading-error")} ${
+            imagesError?.response?.statusText
+          } (${imagesError?.response?.status})`}</Text>
+        )}
         {isErrorAnnotations && (
-          <Text>{`${t("annotations-loading-error")} ${
+          <Text>{`${t("images-loading-error")} ${
             annotationsError?.response?.statusText
           } (${annotationsError?.response?.status})`}</Text>
         )}
-        {isLoadingAnnotations && <Text tx="annotations-loading" />}
-        {annotations && (
-          <List>
-            {annotations?.map((annotation) => (
+        {isLoadingImages && <Text tx="images-loading" />}
+        {images && !isErrorAnnotations && (
+          <ScrollableList>
+            {images?.map((image) => (
               <ClickableListItem
                 onClick={() =>
-                  navigate(editorPath(annotation.image, annotation.id))
+                  navigate(editorPath(image.id, findAnnotationId(image.id)))
                 }
               >
-                {annotation.dataUri}
+                {image.dataUri}
+                <Spacer />
+                {imagesWithAnnotations?.includes(image.id) && (
+                  <AnnotationStatus
+                    color="greenBorder"
+                    borderColor="greenBorder"
+                  />
+                )}
               </ClickableListItem>
             ))}
-          </List>
+          </ScrollableList>
         )}
       </StyledPopUp>
     );
