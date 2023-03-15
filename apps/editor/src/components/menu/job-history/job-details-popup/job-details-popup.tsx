9a480602d1bc31e07cab2954082a7c93e174d403
Tim Riedel <timriedel00@gmail.com>
2023-03-15 15:11:49 +0100

    feat: display annotations on job details popup

--- apps/editor/src/components/menu/job-history/job-details-popup/job-details-popup.tsx
+++ apps/editor/src/components/menu/job-history/job-details-popup/job-details-popup.tsx
@@ -1,6 +1,15 @@
-import { PopUp } from "@visian/ui-shared";
+import {
+  fontSize,
+  List,
+  ListItem,
+  PopUp,
+  Subtitle,
+  Text,
+  useTranslation,
+} from "@visian/ui-shared";
 import { observer } from "mobx-react-lite";
 import styled from "styled-components";
+import { useAnnotationsByJob } from "../../../../queries";
 import { Job } from "../../../../types";
 import { JobDetailsPopUpProps } from "./job-details-popup.props";
 
@@ -9,15 +18,43 @@ const StyledPopUp = styled(PopUp)`
   width: 45vw;
 `;
 
+const StyledSubtitle = styled(Subtitle)`
+  font-size: ${fontSize("navigation")};
+`;
+
 export const JobDetailsPopUp = observer<JobDetailsPopUpProps>(
   ({ job, isOpen, onClose }) => {
+    const {
+      annotations,
+      annotationsError,
+      isErrorAnnotations,
+      isLoadingAnnotations,
+    } = useAnnotationsByJob(job.id);
+
+    const { t } = useTranslation();
+
     return (
       <StyledPopUp
         titleTx="job-details"
         isOpen={isOpen}
         dismiss={onClose}
         shouldDismissOnOutsidePress
-      ></StyledPopUp>
+      >
+        <StyledSubtitle tx={"annotations-generated"} />
+        {isErrorAnnotations && (
+          <Text>{`${t("annotations-loading-error")} ${
+            annotationsError?.response?.statusText
+          } (${annotationsError?.response?.status})`}</Text>
+        )}
+        {isLoadingAnnotations && <Text tx="annotations-loading" />}
+        {annotations && (
+          <List>
+            {annotations?.map((annotation) => (
+              <ListItem>{annotation.dataUri}</ListItem>
+            ))}
+          </List>
+        )}
+      </StyledPopUp>
     );
   },
 );
