05e05e8ac7ab8a60d9532ca029e821a4ef22cad2
Johanna Schlimme <johanna.schlimme@gmx.de>
2023-05-17 17:02:31 +0200

    feat: add button for dataset creation

---
+++ apps/editor/src/components/data-manager/dataset-creation-popup/dataset-creation-popup.tsx
@@ -0,0 +1,72 @@
+import { ButtonParam, PopUp, TextField } from "@visian/ui-shared";
+import { observer } from "mobx-react-lite";
+import { useCallback, useState } from "react";
+import styled from "styled-components";
+
+import { DatasetCreationPopupProps } from "./dataset-creation-popup.props";
+
+const StyledTextButton = styled(ButtonParam)`
+  margin: 0px;
+  width: auto;
+`;
+
+const InlineRow = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 2rem;
+  width: 100%;
+  margin-top: 30px;
+`;
+
+const DatasetCreationPopupContainer = styled(PopUp)`
+  align-items: left;
+  width: 400px;
+`;
+
+const TextInput = styled(TextField)`
+  margin: 0px 0px 0px 0px;
+  width: calc(100% - 40px);
+`;
+
+export const DatasetCreationPopup = observer<DatasetCreationPopupProps>(
+  ({ isOpen, onClose, onConfirm }) => {
+    const [name, setName] = useState("");
+
+    const clearInputsAndClose = useCallback(() => {
+      setName("");
+      onClose?.();
+    }, [onClose]);
+
+    return (
+      <DatasetCreationPopupContainer
+        titleTx="create-dataset"
+        isOpen={isOpen}
+        dismiss={clearInputsAndClose}
+        shouldDismissOnOutsidePress
+      >
+        <>
+          <TextInput
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholderTx="dataset-name"
+          />
+          <InlineRow>
+            <StyledTextButton
+              labelTx="cancel"
+              handlePress={clearInputsAndClose}
+            />
+            <StyledTextButton
+              labelTx="create"
+              handlePress={() => {
+                if (name !== "") {
+                  onConfirm?.({ name });
+                }
+                clearInputsAndClose();
+              }}
+            />
+          </InlineRow>
+        </>
+      </DatasetCreationPopupContainer>
+    );
+  },
+);
