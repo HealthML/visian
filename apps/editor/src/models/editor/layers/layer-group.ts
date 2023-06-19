63d869c77a0a9244e868dcb7909e149eab3f6fb3
Daniel dJ <daniel.dejonge@student.hpi.uni-potsdam.de>
2023-06-19 16:55:52 +0200

    refactor: lint

--- apps/editor/src/models/editor/layers/layer-group.ts
+++ apps/editor/src/models/editor/layers/layer-group.ts
@@ -1,10 +1,13 @@
 import { IDocument, ILayer, ILayerGroup } from "@visian/ui-shared";
 import { ISerializable } from "@visian/utils";
 import { action, makeObservable, observable, toJS } from "mobx";
+
 import { Layer, LayerSnapshot } from "./layer";
+
 export interface LayerGroupSnapshot extends LayerSnapshot {
   layerIds: string[];
 }
+
 export class LayerGroup
   extends Layer
   implements ILayerGroup, ISerializable<LayerGroupSnapshot>
@@ -12,18 +15,23 @@ export class LayerGroup
   // eslint-disable-next-line @typescript-eslint/member-ordering
   public static readonly kind = "group";
   public readonly kind = "group";
+
   protected layerIds: string[] = [];
+
   constructor(
     snapshot: Partial<LayerGroupSnapshot> | undefined,
     protected document: IDocument,
   ) {
     super(snapshot, document);
+
     makeObservable<this, "layerIds">(this, {
       layerIds: observable,
+
       addLayer: action,
       removeLayer: action,
     });
   }
+
   public get layers(): ILayer[] {
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     return this.layerIds.map((id) => this.document.getLayer(id)!);
@@ -51,6 +59,7 @@ export class LayerGroup
     this.layerIds = this.layerIds.filter((id) => id !== idOrLayer.id);
     idOrLayer.setParent();
   }
+
   // Serialization
   public toJSON(): LayerGroupSnapshot {
     return {
