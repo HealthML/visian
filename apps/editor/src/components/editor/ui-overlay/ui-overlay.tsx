c31083116f4debfd5c6dc7e80c74bdfa4271962b
Janis Wehen <janiswehen@gmail.com>
2023-06-27 22:29:55 +0200

    fix: lint

--- apps/editor/src/components/editor/ui-overlay/ui-overlay.tsx
+++ apps/editor/src/components/editor/ui-overlay/ui-overlay.tsx
@@ -14,11 +14,6 @@ import styled from "styled-components";
 
 import { useStore } from "../../../app/root-store";
 import { whoHome } from "../../../constants";
-import { importFilesToDocument } from "../../../import-handling";
-import {
-  fetchAnnotationFile,
-  fetchImageFile,
-} from "../../../queries/use-files";
 import {
   DilateErodeModal,
   MeasurementModal,
