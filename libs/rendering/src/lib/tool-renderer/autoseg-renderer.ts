5208ee9c27bcae0b8f41b705871aaeddb1d404e6
Richard Keil <8680858+richartkeil@users.noreply.github.com>
2023-07-21 15:51:07 +0200

    Make 3D preview slice render code more readable

--- libs/rendering/src/lib/tool-renderer/autoseg-renderer.ts
+++ libs/rendering/src/lib/tool-renderer/autoseg-renderer.ts
@@ -36,36 +36,46 @@ export class AutoSegRenderer extends ToolRenderer3D {
     if (!this.dataTexture) this.initDataTexture();
     const currentSlice = this.document.viewport2D.getSelectedSlice();
     const viewType = this.document.viewport2D.mainViewType;
-
     const { width, height } = this.renderTarget;
-
     const { data } = this.dataTexture.image;
-    for (let i = 0; i < mask.length; i++) {
-      const value = mask[i] > 0.0 ? 255 : 0;
-      let offset = 0;
 
-      // The 3D texture is stored in transverse orientation, while
-      // the incoming mask depends on the current view type, so we
-      // need to figure out the correct offset:
-      if (viewType === ViewType.Transverse) {
-        const x = i % width;
-        const y = Math.floor(i / width);
-        offset = currentSlice * width * height + y * width + x;
-      } else if (viewType === ViewType.Coronal) {
-        const x = i % width;
-        const z = Math.floor(i / width);
-        offset = z * height * width + currentSlice * width + x;
-      } else if (viewType === ViewType.Sagittal) {
-        const x = i % height;
-        const z = Math.floor(i / height);
-        offset = z * height * width + x * width + currentSlice;
+    // The 3D texture is stored in transverse orientation, while
+    // the incoming mask depends on the current view type, so we
+    // need to figure out the correct offset.
+    // Returns offset in the 3D texture, independent of given view.
+    const calculateOffset = (i: number, view: ViewType) => {
+      const sliceArea = width * height;
+
+      let x = 0;
+      let y = 0;
+      let z = 0;
+
+      if (view === ViewType.Transverse) {
+        x = i % width;
+        y = Math.floor(i / width);
+        z = currentSlice;
+      } else if (view === ViewType.Coronal) {
+        x = i % width;
+        y = currentSlice;
+        z = Math.floor(i / width);
+      } else if (view === ViewType.Sagittal) {
+        x = currentSlice;
+        y = i % height;
+        z = Math.floor(i / height);
       }
 
+      return z * sliceArea + y * width + x;
+    };
+
+    for (let i = 0; i < mask.length; i++) {
+      const value = mask[i] > 0.0 ? 255 : 0;
+      const offset = calculateOffset(i, viewType);
       data[offset * 4] = value;
       data[offset * 4 + 1] = value;
       data[offset * 4 + 2] = value;
       data[offset * 4 + 3] = value;
     }
+
     this.dataTexture.needsUpdate = true;
     this.material.setSourceTexture(this.dataTexture);
   }
