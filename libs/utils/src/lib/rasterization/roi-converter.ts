b617518dc72c512b83c336dbe2ceb43eab939e23
Tim Riedel <timriedel00@gmail.com>
2024-01-25 19:39:45 +0100

    feat: fill contours of annotation slices (incl. donut shape)

---
+++ libs/utils/src/lib/rasterization/roi-converter.ts
@@ -0,0 +1,59 @@
+import cv from "@techstark/opencv-js";
+
+/**
+ * Finds all contours in an annotation slice and fills them.
+ * @param annotationSlice Slice of one annotation layer (2D).
+ * @param width Width of the slice.
+ * @param height Height of the slice.
+ * @returns The slice with filled contours.
+ */
+export function fillContours(
+  annotationSlice: Uint8Array,
+  width: number,
+  height: number,
+): Uint8Array {
+  const contours = new cv.MatVector();
+  const hierarchy = new cv.Mat();
+  const slice = cv.matFromArray(width, height, cv.CV_8UC1, annotationSlice);
+  const filledSlice = cv.Mat.zeros(slice.rows, slice.cols, cv.CV_8UC1);
+
+  // Find only the outer contours in the binary image
+  cv.findContours(
+    slice,
+    contours,
+    hierarchy,
+    cv.RETR_EXTERNAL,
+    cv.CHAIN_APPROX_NONE,
+  );
+
+  // Fill the outer contours completely
+  cv.drawContours(filledSlice, contours, -1, new cv.Scalar(255), cv.FILLED);
+
+  // Find contours all contours with hierarchy (incl. inner contours)
+  cv.findContours(
+    slice,
+    contours,
+    hierarchy,
+    cv.RETR_TREE,
+    cv.CHAIN_APPROX_SIMPLE,
+  );
+
+  for (let i = 0; i < contours.size(); ++i) {
+    // Check if the contour has no child (-1)
+    // Hierarchy format: [c1_next, c1_previous, c1_firstChild, c1_parent, c2_next, ...]
+    const child = hierarchy.data32S[i * hierarchy.channels() + 2];
+    if (child < 0) {
+      // fill the contour with black color, but this erases the border
+      cv.drawContours(filledSlice, contours, i, new cv.Scalar(0), -1);
+      // draw the border again
+      cv.drawContours(filledSlice, contours, i, new cv.Scalar(255), 1);
+    }
+  }
+
+  // Release allocated memory
+  slice.delete();
+  contours.delete();
+  hierarchy.delete();
+
+  return filledSlice.data;
+}
