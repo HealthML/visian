579edeedecfd361ca1a37ad246638218deb32f7b
Raban Gießler <raban.giessler@student.hpi.de>
2024-02-05 16:56:48 +0100

    add: drawROIS to post processing

--- apps/editor/src/models/review-strategy/dv-review-task.ts
+++ apps/editor/src/models/review-strategy/dv-review-task.ts
@@ -2,7 +2,12 @@ import {
   DVAnnotationLayer,
   DVAnnotationTask,
   DVAnnotationTaskSnapshot,
+  ViewType,
   createFileFromBase64,
+  drawContours,
+  fillContours,
+  findContours,
+  getPlaneAxes,
   putDVTask,
 } from "@visian/utils";
 import { AxiosResponse } from "axios";
@@ -10,7 +15,7 @@ import { AxiosResponse } from "axios";
 import { ReviewTask, TaskType } from "./review-task";
 import { ImageLayer, Document } from "../editor";
 import { AnnotationGroup } from "../editor/annotation-groups";
-import { IAnnotationGroup } from "@visian/ui-shared";
+import { IAnnotationGroup, IImageLayer } from "@visian/ui-shared";
 
 export interface DVReviewTaskSnapshot {
   dvAnnotationTaskSnap: DVAnnotationTaskSnapshot;
@@ -61,8 +66,6 @@ export class DVReviewTask extends ReviewTask {
     return [createFileFromBase64("DVimage", this.dvTask.scan.data)];
   }
 
-  public addGroup() {}
-
   public async getAnnotationFiles(annotationId: string) {
     return [];
   }
@@ -70,12 +73,47 @@ export class DVReviewTask extends ReviewTask {
   public addGroupsAndLayers(document: Document) {
     //TODO: remove default group
 
-    // const defaultGroup = document.annotationGroups[0];
-    // if (!defaultGroup) throw new Error("No default group");
+    const map = this.getDvVisianLayerMapping(document);
+    const roisList = this.dvTask.getLayerRoisList();
 
-    for (const layer of this.dvTask.annotationLayers) {
-      this.addLayerFromAnnotation(layer, document);
+    for (const rois of roisList) {
+      const visianLayerId = map.get(rois.layerID);
+      if (!visianLayerId) throw new Error("No visian layer");
+      const layer = document.getLayer(visianLayerId) as IImageLayer;
+      if (!layer || !layer.isAnnotation) throw new Error("No layer");
+      this.drawROIS(rois.rois, layer, rois.z);
     }
+  }
+
+  private getDvVisianLayerMapping(document: Document): Map<string, string> {
+    var map = new Map<string, string>();
+    for (const dvLayer of this.dvTask.annotationLayers) {
+      const visianLayer = this.addLayerFromAnnotation(dvLayer, document);
+      map.set(dvLayer.annotationID, visianLayer.id);
+    }
+    return map;
+  }
+
+  private drawROIS(rois: number[][], layer: IImageLayer, z: number) {
+    const [widthAxis, heightAxis] = getPlaneAxes(ViewType.Transverse);
+    const width = layer.image.voxelCount[widthAxis];
+    const height = layer.image.voxelCount[heightAxis];
+
+    const intRois = [];
+    for (let i = 0; i < rois.length; i++) {
+      const roi = rois[i];
+      const intRoi = new Int32Array(roi.length);
+      for (let j = 0; j < roi.length; j++) {
+        intRoi[j] = Math.round(roi[j]);
+      }
+      intRois.push(intRoi);
+    }
+
+    // eslint-disable-next-line no-console
+    console.log("roi coordinates as loaded from file", intRois);
+    const data = drawContours(intRois, width, height);
+    layer.setSlice(ViewType.Transverse, z, data);
+    layer.recomputeSliceMarkers();
   }
 
   private addNewGroup(title: string, document: Document): IAnnotationGroup {
@@ -87,9 +125,10 @@ export class DVReviewTask extends ReviewTask {
   private addLayerFromAnnotation(
     dvLayer: DVAnnotationLayer,
     document: Document,
-  ) {
+  ): ImageLayer {
     if (!document.mainImageLayer) throw new Error("No main image layer");
 
+    //TODO: add correct color
     const layer = ImageLayer.fromNewAnnotationForImage(
       document.mainImageLayer.image,
       document,
@@ -100,6 +139,8 @@ export class DVReviewTask extends ReviewTask {
     document.addLayer(layer);
     const group = this.getGroupFromLayer(dvLayer, document);
     group.addLayer(layer.id);
+
+    return layer;
   }
 
   private getGroupFromLayer(
