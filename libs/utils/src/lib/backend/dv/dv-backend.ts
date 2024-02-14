e9343c5b5823d630ddcd0dae755e2d5afdb3c9f1
Raban Gießler <raban.giessler@student.hpi.de>
2024-02-14 14:29:10 +0100

    chg: rename dvTask to dvAnnotationTask

--- libs/utils/src/lib/backend/dv/dv-backend.ts
+++ libs/utils/src/lib/backend/dv/dv-backend.ts
@@ -3,13 +3,18 @@ import { DVAnnotationTask } from "./types";
 
 export const dvBackendBaseUrl = "https://annotation.ai4h.net/api/v1";
 
-export const getDVTask = async (taskId: string): Promise<DVAnnotationTask> => {
+export const getDvAnnotationTask = async (
+  taskId: string,
+): Promise<DVAnnotationTask> => {
   // TODO: implement getDVTask
   const task = DVAnnotationTask.createFromImport(jsonData);
   return task;
 };
 
-export const putDVTask = async (taskId: string, task: DVAnnotationTask) => {
+export const putDvAnnotationTask = async (
+  taskId: string,
+  task: DVAnnotationTask,
+) => {
   // TODO: implement putDVTask
   console.log("Export Task to console");
   console.log(task);
