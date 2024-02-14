ff3d38e6b9783c0b52445d846aca14d4a0b25e29
Raban Gießler <raban.giessler@student.hpi.de>
2024-02-14 10:35:05 +0100

    fix: linting issues

--- libs/utils/src/lib/backend/dv/dv-backend.ts
+++ libs/utils/src/lib/backend/dv/dv-backend.ts
@@ -4,15 +4,13 @@ import { DVAnnotationTask } from "./types";
 export const dvBackendBaseUrl = "https://annotation.ai4h.net/api/v1";
 
 export const getDVTask = async (taskId: string): Promise<DVAnnotationTask> => {
-  //TODO: implement getDVTask
-
+  // TODO: implement getDVTask
   const task = DVAnnotationTask.createFromImport(jsonData);
   return task;
 };
 
 export const putDVTask = async (taskId: string, task: DVAnnotationTask) => {
-  //TODO: implement putDVTask
-
+  // TODO: implement putDVTask
   console.log("Export Task to console");
   console.log(task);
 };
